8994cccfca2d1b367640c44c6a3ed34b540fb55d
Jeff W <Jeff@fraction.rent>
2020-03-31 05:39:34 -0400

    Updates

---
+++ precache-manifest.689767ea6d56c060439979f138be673b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd421cefbc9bfbcc87c8d0ff22826777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9cd94e27128862da1a9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35d41aac00df15ce6753",
+    "url": "/static/js/2.26d3bfd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26d3bfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9cd94e27128862da1a9",
+    "url": "/static/js/main.747b35dd.chunk.js"
+  },
+  {
+    "revision": "5331463223af78c92be9",
+    "url": "/static/js/runtime-main.efbea74f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rap-tv/precache-manifest.468f0c405b2e2803113a9dc8c640afaa.js"
+  "/precache-manifest.689767ea6d56c060439979f138be673b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rap-tv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
